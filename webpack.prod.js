1665c439cedfd6eda6fcc6a2675ef5612a10eb92
Doug Ebersole <debersole81@gmail.com>
2024-06-18 16:25:46 -0400

    Add html-loader, file-loader, and clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,5 +7,7 @@ module.exports = merge(common, {
   output: {
     filename: 'main.[hash].js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[name][ext]',
+    clean: true,
   },
 });
